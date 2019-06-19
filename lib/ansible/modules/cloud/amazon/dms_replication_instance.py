8c3debd6535ccf3ea962290bdeab117cceb295fe
Rui  Moreira <ruimoreira29@gmail.com>
2019-06-19 20:56:34 +0100

    - added shorter version of gpl - minor adjustments to avoid using global vars

--- lib/ansible/modules/cloud/amazon/dms_replication_instance.py
+++ lib/ansible/modules/cloud/amazon/dms_replication_instance.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright: Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 
 ANSIBLE_METADATA = {'metadata_version': '1.0',
                     'status': ['preview'],
@@ -32,21 +21,26 @@ options:
         - State of the endpoint
       default: present
       choices: ['present', 'absent']
+      type: str
     identifier:
+      description:
         - Identifier for the instance
+      type: str
     allocatedstorage:
-        description: 
-          - The amount of storage (in gigabytes) to be initially 
+        description:
+          - The amount of storage (in gigabytes) to be initially
             allocated for the replication instance
+        type: int
     instanceclass:
-        description: 
-          - The compute and memory capacity of the replication 
+        description:
+          - The compute and memory capacity of the replication
             instance as specified by the replication instance class.
         choices: ['dms.t2.micro', 'dms.t2.small','dms.t2.medium', 'dms.t2.large',
                  'dms.c4.large', 'dms.c4.xlarge','dms.c4.2xlarge', 'dms.c4.4xlarge'])
+        type: str
     vpcsecuritygroupids:
         description:
-          - Specifies the VPC security group to be used with the replication instance. 
+          - Specifies the VPC security group to be used with the replication instance.
             The VPC security group must work with the VPC containing the replication instance.
     availabilityzone:
         description:
@@ -56,7 +50,7 @@ options:
           -  A subnet group to associate with the replication instance.
     preferredmaintenancewindow:
         description:
-          - The weekly time range during which system maintenance can occur, 
+          - The weekly time range during which system maintenance can occur,
             in Universal Coordinated Time (UTC). Format: ddd:hh24:mi-ddd:hh24:mi
     multiaz:
         description:
@@ -67,17 +61,17 @@ options:
           - The engine version number of the replication instance.
     autominorversionupgrade:
         description:
-          - Indicates that minor engine upgrades will be applied automatically 
+          - Indicates that minor engine upgrades will be applied automatically
           to the replication instance during the maintenance window.
         type: bool
         default: 'true'
     kmskeyid:
         description:
-          - The AWS KMS key identifier that is used to encrypt the content on the replication instance. 
+          - The AWS KMS key identifier that is used to encrypt the content on the replication instance.
           If you don't specify a value for the KmsKeyId parameter, then AWS DMS uses your default encryption key
     publiclyaccessible:
         description:
-          - Specifies the accessibility options for the replication instance. 
+          - Specifies the accessibility options for the replication instance.
           A value of true represents an instance with a public IP address
     dnsnameservers:
         description:
@@ -99,6 +93,8 @@ except ImportError:
     pass  # caught by AnsibleAWSModule
 
 backoff_params = dict(tries=5, delay=1, backoff=1.5)
+
+
 @AWSRetry.backoff(**backoff_params)
 def describe_instance(connection, instance_identifier):
     """checks if instance exists"""
@@ -110,7 +106,7 @@ def describe_instance(connection, instance_identifier):
         return {'ReplicationInstances': []}
 
 
-def get_dms_client(aws_connect_params, client_region, ec2_url):
+def get_dms_client(module, aws_connect_params, client_region, ec2_url):
     client_params = dict(
         module=module,
         conn_type='client',
@@ -129,45 +125,46 @@ def instance_exists(instance):
     """
     return bool(len(instance['ReplicationInstances']))
 
-def create_module_params():
+
+def create_module_params(module):
     """
     Reads the module parameters and returns a dict
     :return: dict
     """
     instance_parameters = dict(
-        ReplicationInstanceIdentifier = module.params.get('identifier'),
-        AllocatedStorage = module.params.get('allocatedstorage'),
+        ReplicationInstanceIdentifier=module.params.get('identifier'),
+        AllocatedStorage=module.params.get('allocatedstorage'),
     )
 
     return instance_parameters
 
+
 def main():
     argument_spec = dict(
-        state=dict(choices=['present', 'absent'], default='present'),
-        identifier=dict(required=True),
+        state=dict(type='str', choices=['present', 'absent'], default='present'),
+        identifier=dict(type='str', required=True),
         allocatedstorage=dict(type='int'),
         instanceclass=dict(required=True, choices=['dms.t2.micro', 'dms.t2.small',
                                                    'dms.t2.medium', 'dms.t2.large',
                                                    'dms.c4.large', 'dms.c4.xlarge',
                                                    'dms.c4.2xlarge', 'dms.c4.4xlarge']),
-        vpcsecuritygroupids=dict(type='list'),
+        securitygroups=dict(type='list'),
         availabilityzone=dict(),
-        subnetgroupidentifier=dict(),
-        preferredmaintenancewindow=dict(),
+        subnetgroup=dict(),
+        maintenancewindow=dict(),
         multiaz=dict(type='bool', default=False),
         engineversion=dict(),
-        autominorversionupgrade=dict(type='bool', default=True),
+        versionupgrade=dict(type='bool', default=True),
         kmskeyid=dict(),
-        publiclyaccessible=dict(type='bool', default=True),
-        dnsnameservers=dict(),
+        public=dict(type='bool', default=True),
+        nameservers=dict(),
         tags=dict(type='list'),
 
     )
-    global module
     module = AnsibleAWSModule(
         argument_spec=argument_spec,
         required_if=[],
-        supports_check_mode=False
+        supports_check_mode=True
     )
     exit_message = None
     changed = False
@@ -176,13 +173,23 @@ def main():
     state = module.params.get('state')
     aws_config_region, ec2_url, aws_connect_params = \
         get_aws_connection_info(module, boto3=True)
-    dmsclient = get_dms_client(aws_connect_params, aws_config_region, ec2_url)
+    dmsclient = get_dms_client(module, aws_connect_params, aws_config_region, ec2_url)
     replication_instance = describe_instance(dmsclient,
                                              module.params.get('identifier'))
     if state == 'present':
         if instance_exists(replication_instance):
-            module.exit_json(changed=changed, msg=argument_spec)
+            if module.check_mode:
+                changed = True
+                msg="Check mode enabled"
+            else:
+                changed = True
+                msg="Instance Updated"
+
+            module.exit_json(changed=changed, msg=replication_instance)
+        else:
+            module.exit_json(changed=changed, msg=replication_instance)
     module.exit_json(changed=changed, msg=exit_message)
+
 
 if __name__ == '__main__':
     main()
